a2cd0bbdb509d5925f231f0af62ceb40c779fe69
lemon9527 <lliu3@fellowes.com>
2026-07-06 14:48:33 +0800

    Remove .DS_Store files

---
+++ 04.peripheral/basics/gpio/main/main.c
@@ -0,0 +1,21 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include <stdio.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define LED_GPIO_PIN 2
+
+static const char *TAG = "task_create_main";
+
+
+void app_main(void)
+{
+
+}
